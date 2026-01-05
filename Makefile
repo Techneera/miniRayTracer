dd6fcc9e3cb2c9e5a4772d8cf5245cae0994992a
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2026-01-05 21:49:34 +0000

    Update: add test build

--- Makefile
+++ Makefile
@@ -1,27 +1,37 @@
-NAME = miniRT
+NAME = $(BDIR)miniRT
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
+LMATH = -lm
 
 # ------------------------------ DIRECTORIES -------------------------------- #
 
 IDIR = include/
 ODIR = obj/
 SDIR = src/
+TDIR = unit_test/
+BDIR = build/
 
 VECDIR = $(SDIR)vec_utils
 
 # ------------------------------ VECTORS -------------------------------- #
 
 _VEC_SRCS = base.c
-VEC_SRCS = $(patsubst %.c, $(VECDIR)/%.c, $(_VECSCRS))
+VEC_SRCS = $(patsubst %.c, $(VECDIR)/%.c, $(_VEC_SCRS))
 
-SRCFILES = $(SDIR)main.c \
-		   $(VECSRCS)
+
+# ------------------------------ UNIT TESTS-------------------------------- #
+
+_OPS_SRCS = vec_ops.c
 
 # ------------------------------ BUILD -------------------------------- #
 
 OBJ = $(patsubst %.c, %.o, $(SRCFILES))
 DEP = $(IDIR)
+
+# ------------------------------ SOURCES -------------------------------- #
+
+SRCFILES = $(SDIR)main.c \
+		   $(VECSRCS)
 
 # ------------------------------ RULES -------------------------------- #
 
@@ -30,10 +40,16 @@ all: $(NAME)
 debug: CFLAGS += -g
 debug: re
 
+vec_test:
+	mkdir -p $(BDIR)
+	$(CC) $(CFLAGS) $(TDIR)$(_OPS_SRCS) -o $(BDIR) -I $(IDIR) $(LMATH)
+
+
 $(NAME): $(OBJ)
 	$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
 
 %.o: %.c
+	mkdir -p $(BDIR)
 	mkdir -p $(ODIR)vec_utils
 	$(CC) $(CFLAGS) -c $< -o $@ -I $(IDIR)
 
@@ -41,8 +57,8 @@ clean:
 	rm -rf $(ODIR)
 
 fclean: clean
-	rm -f $(NAME)
+	rm -rf $(BDIR)
 
 re: fclean all
 
-.PHONY: all clean fclean re debug
+.PHONY: all clean fclean re debug vec_test
--- src/vec_utils/base.c
+++ src/vec_utils/base.c
@@ -1,8 +1,114 @@
 #include "vector.h"
 
-typedef struct s_vec3
+inline
+t_vec3	point_constructor(double x, double y, double z)
 {
-	double	x;
-	double	y;
-	double	z;
-}	t_vec3;
+	t_vec3	result;
+
+	result.x = x;
+	result.y = y;
+	result.z = z;
+	result.w = 1.0;
+	return (result);
+}
+
+inline
+t_vec3	vector_constructor(double x, double y, double z)
+{
+	t_vec3	result;
+
+	result.x = x;
+	result.y = y;
+	result.z = z;
+	result.w = 0.0;
+	return (result);
+}
+
+inline
+double	vector_length_squared(const t_vec3 u)
+{
+	return (u.x * u.x + u.y * u.y + u.z * u.z);
+}
+
+inline
+double	vector_length(const t_vec3 u)
+{
+	return (sqrt(u.x * u.x + u.y * u.y + u.z * u.z));
+}
+
+inline
+t_vec3	vector_normalization(const t_vec3 u)
+{
+	double	len;
+	t_vec3	result;
+
+	len = vector_length(u);
+	if (len > 0.0)
+	{
+		len = 1.0 / len;
+		result.x = u.x * len;
+		result.y = u.y * len;
+		result.z = u.z * len;
+		result.w = u.w;
+		return (result);
+	}
+	result.x = 0.0;
+	result.y = 0.0;
+	result.z = 0.0;
+	result.w = u.w;
+	return (result);
+}
+
+inline
+double	vector_dot_product(const t_vec3 u, const t_vec3 v)
+{
+	return (u.x * v.x + u.y * v.y + u.z * v.z);
+}
+
+inline
+t_vec3	vector_cross_product(const t_vec3 u, const t_vec3 v)
+{
+	t_vec3	result;
+
+	result.x = u.y * v.z - u.z * v.y;
+	result.y = u.z * v.x - u.x * v.z;
+	result.z = u.x * v.y - u.y * v.x;
+	result.w = 0.0;
+	return (result);
+}
+
+inline
+t_vec3	vector_add(const t_vec3 u, const t_vec3 v)
+{
+	t_vec3	result;
+
+	result.x = u.x + v.x;
+	result.y = u.y + v.y;
+	result.z = u.z + v.z;
+	result.w = 0.0;
+	return (result);
+}
+
+inline
+t_vec3	vector_sub(const t_vec3 u, const t_vec3 v)
+{
+	t_vec3	result;
+
+	result.x = u.x - v.x;
+	result.y = u.y - v.y;
+	result.z = u.z - v.z;
+	result.w = 0.0;
+	return (result);
+}
+
+inline
+t_vec3	vector_scale(const t_vec3 u, double scalar)
+{
+	t_vec3	result;
+
+	result.x = u.x * scalar;
+	result.y = u.y * scalar;
+	result.z = u.z * scalar;
+	result.w = u.w;
+	return (result);
+}
