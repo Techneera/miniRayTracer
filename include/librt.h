30693a6f69e1c15d1e4c0b4c516852b4d55ff469
Julio Souza <juliosouza.ux@gmail.com>
2026-01-19 14:47:58 +0000

    Pulled from main and completed the t_scene struct

--- include/librt.h
+++ include/librt.h
@@ -1,37 +1,50 @@
 #ifndef LIBRT_H
 # define LIBRT_H
 
+# include "ray.h"
+#include "vector.h"
+
 # define MAX_OBJECTS 50
 
-//typedef struct s_objects
-//{
-//	union u_objects
-//	{
-//		struct s_sphere;
-//		struct s_plane;
-//		struct s_cylinder;
-//	};
-//} t_objects;
-//
-//typedef struct s_ambient_light
-//{
-//} t_ambient_light;
-//
-//typedef struct s_camera
-//{
-//} t_camera;
-//
-//typedef struct s_light
-//{
-//} t_light;
-//
-//typedef struct s_scene
-//{
-//	t_ambient_light a_light;
-//	t_camera camera;
-//	t_light light;
-//	t_objects objects[MAX_OBJECTS];
-//} t_scene;
+typedef enum e_type
+{
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_type;
+
+typedef struct s_objects
+{
+	t_type	type;
+	t_object object;
+} t_objects;
+
+typedef struct s_ambient_light
+{
+	float	ratio;
+	t_vec3	color;
+} t_ambient_light;
+
+typedef struct s_camera
+{
+	t_vec3	position;
+	t_vec3	direction;
+	int		fov;
+} t_camera;
+
+typedef struct s_light
+{
+	t_vec3	position;
+	float	brightness;
+} t_light;
+
+typedef struct s_scene
+{
+	t_ambient_light a_light;
+	t_camera camera;
+	t_light light;
+	t_objects objects[MAX_OBJECTS];
+} t_scene;
 
 #endif
 
