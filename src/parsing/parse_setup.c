a68bc72270b6dfc71ab6e440af3aaecab00fa80d
Julio Souza <juliosouza.ux@gmail.com>
2026-01-20 16:41:33 +0000

    UPDATE: Refactored parse_ambient_light check

--- src/parsing/parse_setup.c
+++ src/parsing/parse_setup.c
@@ -8,9 +8,8 @@ int	parse_ambient_light(char **line, t_scene *scene)
 	if (scene->a_light.ratio < 0.0 || scene->a_light.ratio > 1.0)
 		return (1);
 	*line = skip_to_next(*line);
-	if (parse_vec3(line, &scene->a_light.color) != 0)
-		return (1);
-	if (is_valid_color(scene->a_light.color) != true)
+	if (parse_vec3(line, &scene->a_light.color) != 0
+		|| is_valid_color(scene->a_light.color) != true)
 		return (1);
 	scene->a_light.color.x = round(scene->a_light.color.x * RGB_FACTOR);
 	scene->a_light.color.y = round(scene->a_light.color.y * RGB_FACTOR);
