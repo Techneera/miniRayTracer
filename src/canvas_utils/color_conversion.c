81303d5fc983394e2e0dcfa124b3166c669c11fa
Rafael Rezende <rafael.yanase@gmail.com>
2026-01-24 13:16:03 +0000

    Update: color to int clamping in case of overflow

--- src/canvas_utils/color_conversion.c
+++ src/canvas_utils/color_conversion.c
@@ -6,6 +6,12 @@ int	color_to_int(t_vec3 color)
 	int	g;
 	int	b;
 
+	if (color.x > 1.0f)
+		color.x = 1.0f;
+	if (color.y > 1.0f)
+		color.y = 1.0f;
+	if (color.z > 1.0f)
+		color.z = 1.0f;
 	r = round(color.x * RGB_MAX);
 	g = round(color.y * RGB_MAX);
 	b = round(color.z * RGB_MAX);
