23a21b88d8f3dccef0010a6c23091138343eb37e
Julio Souza <juliosouza.ux@gmail.com>
2026-01-27 18:22:32 +0000

    add: added t_computation struct

--- include/scene.h
+++ include/scene.h
@@ -4,6 +4,7 @@
 # include "shades.h"
 # include "librt.h"
 # include "ray.h"
+#include "vector.h"
 
 typedef struct s_world
 {
@@ -11,6 +12,14 @@ typedef struct s_world
     t_objects       objects[MAX_OBJECTS];
     int             object_count;
 }   t_world;
+
+typedef struct s_computation
+{
+    float       t;
+    t_object    object;
+    t_vec3      point;
+    t_vec3      normalv;
+}   t_computation;
 
 t_world default_world(void);
 t_intersect intersect_world(t_world *world, t_ray ray);
