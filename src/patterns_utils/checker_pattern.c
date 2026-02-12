9c4d22ba3829a2073c0dfdc8df702723923f1912
Julio Souza <juliosouza.ux@gmail.com>
2026-02-12 10:21:27 +0000

    add: add checker constructor

--- src/patterns_utils/checker_pattern.c
+++ src/patterns_utils/checker_pattern.c
@@ -2,6 +2,11 @@
 #include "vector.h"
 #include <math.h>
 
+t_pattern	pattern_checker(t_vec3 color_a, t_vec3 color_b)
+{
+	return (pattern_constructor(PATTERN_CHECKER, color_a, color_b));
+}
+
 t_vec3 checker_at(t_pattern pattern, t_vec3 point)
 {
 	float	result;
