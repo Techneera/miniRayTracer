f07be32ff46143641494dce1498198d8ce1f987f
Julio Souza <juliosouza.ux@gmail.com>
2026-02-12 10:22:14 +0000

    add: add logic to construct and get gradient pattern color

---
+++ src/patterns_utils/gradient_pattern.c
@@ -0,0 +1,21 @@
+#include "patterns.h"
+#include "vector.h"
+#include <math.h>
+
+t_pattern	pattern_gradient(t_vec3 color_a, t_vec3 color_b)
+{
+	return (pattern_constructor(PATTERN_GRADIENT, color_a, color_b));
+}
+
+t_vec3	gradient_at(t_pattern pattern, t_vec3 point)
+{
+	t_vec3	distance;
+	float	fraction;
+
+	distance = vector_sub(pattern.b, pattern.a);
+	fraction = point.x - floorf(point.x);
+	return (vector_add(
+		pattern.a,
+		vector_scale(distance, fraction)
+	));
+}
